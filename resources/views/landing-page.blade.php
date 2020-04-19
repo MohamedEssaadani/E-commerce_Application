c69cc90720783156eba87578a3afca04eea7477f
Essaadani <essaadani80@gmail.com>
2020-04-19 18:37:42 +0000

    Refactoring Landing Page

--- resources/views/landing-page.blade.php
+++ resources/views/landing-page.blade.php
@@ -11,22 +11,66 @@ Home
     <div id="sequence" class="seq">
       <div class="seq-screen">
         <ul class="seq-canvas">
-          <!-- Slides -->
-          @foreach ($slides as $slide )
+          <!-- single slide item -->
           <li>
             <div class="seq-model">
-              <img data-seq src="{{asset('storage/'.$slide->image)}}" alt="Men slide img" />
+              <img data-seq src="../assets/img/slider/1.jpg" alt="Men slide img" />
             </div>
             <div class="seq-title">
               <span data-seq>Save Up to 75% Off</span>
-              <h2 data-seq>{{$slide->title}}</h2>
-              <p data-seq>
-                <h2 data-seq>{{$slide->subtitle}}</h2>
-              </p>
+              <h2 data-seq>Men Collection</h2>
+              <p data-seq>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minus, illum.</p>
               <a data-seq href="#" class="aa-shop-now-btn aa-secondary-btn">SHOP NOW</a>
             </div>
           </li>
-          @endforeach
+          <!-- single slide item -->
+          <li>
+            <div class="seq-model">
+              <img data-seq src="../assets/img/slider/2.jpg" alt="Wristwatch slide img" />
+            </div>
+            <div class="seq-title">
+              <span data-seq>Save Up to 40% Off</span>
+              <h2 data-seq>Wristwatch Collection</h2>
+              <p data-seq>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minus, illum.</p>
+              <a data-seq href="#" class="aa-shop-now-btn aa-secondary-btn">SHOP NOW</a>
+            </div>
+          </li>
+          <!-- single slide item -->
+          <li>
+            <div class="seq-model">
+              <img data-seq src="../assets/img/slider/3.jpg" alt="Women Jeans slide img" />
+            </div>
+            <div class="seq-title">
+              <span data-seq>Save Up to 75% Off</span>
+              <h2 data-seq>Jeans Collection</h2>
+              <p data-seq>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minus, illum.</p>
+              <a data-seq href="#" class="aa-shop-now-btn aa-secondary-btn">SHOP NOW</a>
+            </div>
+          </li>
+          <!-- single slide item -->
+          <li>
+            <div class="seq-model">
+              <img data-seq src="../assets/img/slider/4.jpg" alt="Shoes slide img" />
+            </div>
+            <div class="seq-title">
+              <span data-seq>Save Up to 75% Off</span>
+              <h2 data-seq>Exclusive Shoes</h2>
+              <p data-seq>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minus, illum.</p>
+              <a data-seq href="#" class="aa-shop-now-btn aa-secondary-btn">SHOP NOW</a>
+            </div>
+          </li>
+          <!-- single slide item -->
+          <li>
+            <div class="seq-model">
+              <img data-seq src="../assets/img/slider/5.jpg" alt="Male Female slide img" />
+            </div>
+            <div class="seq-title">
+              <span data-seq>Save Up to 50% Off</span>
+              <h2 data-seq>Best Collection</h2>
+              <p data-seq>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minus, illum.</p>
+              <a data-seq href="#" class="aa-shop-now-btn aa-secondary-btn">SHOP NOW</a>
+            </div>
+          </li>
         </ul>
       </div>
       <!-- slider navigation btn -->
@@ -37,22 +81,21 @@ Home
     </div>
   </div>
 </section>
-<!-- / slider -->
-
+<!--Slides-->
 <!-- Products section -->
-<section id="aa-product">
+<section id="aa-product" style="margin-top:40px;">
   <div class="container">
     <div class="row">
       <div class="col-md-12">
         <div class="row">
           <div class="aa-product-area">
             <div class="aa-product-inner">
-              <!-- start prduct navigation -->
-              <h2 style="text-align:center; margin-bottom:20px; margin-top:20px;">Featured Products</h2>
+
               <!-- Tab panes -->
               <div class="tab-content">
                 <!-- start men product category -->
                 <div class="tab-pane fade in active" id="men">
+                  <h2>Feautered Products</h2>
                   <ul class="aa-product-catg">
                     <!-- start single product item -->
                     @foreach($products as $product)
@@ -68,7 +111,14 @@ Home
                             class="aa-product-price"><del>${{$product->price*2}}</del></span>
                         </figcaption>
                       </figure>
-
+                      <div class="aa-product-hvr-content">
+                        <a href="#" data-toggle="tooltip" data-placement="top" title="Add to Wishlist"><span
+                            class="fa fa-heart-o"></span></a>
+                        <a href="#" data-toggle="tooltip" data-placement="top" title="Compare"><span
+                            class="fa fa-exchange"></span></a>
+                        <a href="#" data-toggle2="tooltip" data-placement="top" title="Quick View" data-toggle="modal"
+                          data-target="#quick-view-modal"><span class="fa fa-search"></span></a>
+                      </div>
                       <!-- product badge -->
                       <span class="aa-badge aa-sale" href="#">SALE!</span>
                     </li>
@@ -79,15 +129,11 @@ Home
                       class="fa fa-long-arrow-right"></span></a>
                 </div>
               </div>
+
+
             </div>
-          </div>
-        </div>
-      </div>
-    </div>
-  </div>
 </section>
 <!-- / Products section -->
-
 
 <!-- Support section -->
 <section id="aa-support">
@@ -100,7 +146,7 @@ Home
             <div class="aa-support-single">
               <span class="fa fa-truck"></span>
               <h4>FREE SHIPPING</h4>
-              <P>We provide FREE SHIPPING for worldwide.</P>
+              <P>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quam, nobis.</P>
             </div>
           </div>
           <!-- single support -->
@@ -108,7 +154,7 @@ Home
             <div class="aa-support-single">
               <span class="fa fa-clock-o"></span>
               <h4>30 DAYS MONEY BACK</h4>
-              <P>If you make a refund or face a problem your will get your money before 30 Days.</P>
+              <P>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quam, nobis.</P>
             </div>
           </div>
           <!-- single support -->
@@ -116,7 +162,7 @@ Home
             <div class="aa-support-single">
               <span class="fa fa-phone"></span>
               <h4>SUPPORT 24/7</h4>
-              <P>Call us anytime we are available 24H/7 Days.</P>
+              <P>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quam, nobis.</P>
             </div>
           </div>
         </div>
@@ -125,7 +171,6 @@ Home
   </div>
 </section>
 <!-- / Support section -->
-
 
 <!-- Latest Blog -->
 <section id="aa-latest-blog">
